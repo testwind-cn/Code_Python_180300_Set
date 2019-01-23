ccfaf286b910c9f219d916157607904e4c2c61c5
acewind1 <36976754+acewind1@users.noreply.github.com>
2019-01-23 10:19:02 +0800

    20190123-修改文件夹名
    shouyinbao 修改成 posflow

---
+++ posflow/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+from wj_tools import  sftp_tool
+
+if __name__ == "__main__":
+
+    a = sftp_tool.Sftp_Tool( h = '172.31.130.14', p = 22, u = 'root', s = 'Redhat@2016', r = '/ftpdata/thblposloan/posflow/', d = 'C:\\Users\\wangjun\\Desktop\\python\\posflow')
+    a.openSFTP()
+    ll = a.getRmFilesList('/ftpdata/thblposloan/posflow/')
+    ll = a.getLcFilesList('C:\\Users\\wangjun\\Desktop\\python\\posflow')
