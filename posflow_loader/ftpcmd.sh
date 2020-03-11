a7d5e1c1af4efec22336c80be2abfe1fc94f6738
acewind1 <36976754+acewind1@users.noreply.github.com>
2020-03-11 12:42:19 +0800

    200310 - 修改 FTP 密码

--- posflow_loader/ftpcmd.sh
+++ posflow_loader/ftpcmd.sh
@@ -2,7 +2,8 @@
 # mirror -P=10 /9999110000/201811/08/ /home/data1 --log=./ftplog/lftp_log_${DATES}.txt
 
 DATES=$(date -d "-0 day" +%Y%m%d%H%M%S)
-lftp -u thbl:ThBl2016 172.16.138.93 <<EOF
+# lftp -u thbl:ThBl2016 172.16.138.93 <<EOF
+lftp -u thzc,'Th,12019!Zc' 172.16.138.93 <<EOF
 mirror -P=10 / /home/data/SYB/ --log=./ftplog/lftp_log_${DATES}.txt
 close
 bye
